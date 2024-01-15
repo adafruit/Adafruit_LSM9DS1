12d858cf7d8587f1fbdb0e7f9e2e4d23677164b7
Doxygen CI <ci-arduino@invalid>
2024-01-15 21:54:47 +0000

    Deploy docs to GitHub Pages from commit d5248609f2
    Commit: d5248609f2a9c78bcd6040e0a1ded2d64aa2004c
    GitHub Actions run: 7534316736

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['sensor',['Sensor',['../class_adafruit___l_s_m9_d_s1_1_1_sensor.html#a5fc3b59cd2f125f6c08ea3f4acd6f983',1,'Adafruit_LSM9DS1::Sensor::Sensor()'],['../class_adafruit___l_s_m9_d_s1_1_1_sensor.html#adda2a21206d567ca45ddb0abc2c3bc4c',1,'Adafruit_LSM9DS1::Sensor::Sensor(const Sensor &amp;copy)'],['../class_adafruit___l_s_m9_d_s1_1_1_sensor.html#a07aed32ba2ca30b421e36ac38d0298df',1,'Adafruit_LSM9DS1::Sensor::Sensor(Adafruit_LSM9DS1 *parent, lsm9ds1_read_func readFunc, lsm9ds1_get_event_func eventFunc, lsm9ds1_get_sensor_func sensorFunc)']]],
-  ['setupaccel',['setupAccel',['../class_adafruit___l_s_m9_d_s1.html#a9c81eb885270b9ca3b0ae64ee9283ffe',1,'Adafruit_LSM9DS1']]],
+  ['setupaccel',['setupAccel',['../class_adafruit___l_s_m9_d_s1.html#a34c98df34b9df7ef5ad0ded7351db64e',1,'Adafruit_LSM9DS1']]],
   ['setupgyro',['setupGyro',['../class_adafruit___l_s_m9_d_s1.html#aa7f75b905c5f96fc5bdb82128d8ad748',1,'Adafruit_LSM9DS1']]],
   ['setupmag',['setupMag',['../class_adafruit___l_s_m9_d_s1.html#ace604ddc6a73ca3d1ab880e0d3e686d6',1,'Adafruit_LSM9DS1']]]
 ];
